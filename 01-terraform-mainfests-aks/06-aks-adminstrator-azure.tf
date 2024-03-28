fb173f543d42bd303153d3d294c0c4f4cfd39d77
Devon Li <jlix720@gmail.com>
2024-03-29 02:17:08 +1000

    add env

--- 01-terraform-mainfests-aks/06-aks-adminstrator-azure.tf
+++ 01-terraform-mainfests-aks/06-aks-adminstrator-azure.tf
@@ -1,6 +1,6 @@
 resource "azuread_group" "aks_administrators" {
 
-  display_name     = "${azurerm_resource_group.aks_rg.name}-cluster-administrators"
+  display_name     = "${azurerm_resource_group.aks_rg.name}-${var.environment}-administrators"
   security_enabled = true
   description      = "Azure AKS Kubernetes administrator for the ${azurerm_resource_group.aks_rg.name}-cluster"
 
